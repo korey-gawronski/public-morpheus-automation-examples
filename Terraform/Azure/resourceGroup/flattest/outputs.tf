a39c109c8177cdcc16347e8210bea0e66421d7a6
Korey Gawronski <korey.gawronski@hpe.com>
2026-04-13 15:01:30 -0400

    Fixed references

--- Terraform/Azure/resourceGroup/flattest/outputs.tf
+++ Terraform/Azure/resourceGroup/flattest/outputs.tf
@@ -1,18 +1,3 @@
-output "rgid" {
-  description = "ARN of the bucket"
-  value       = module.rg.id
-}
-
-output "rgname" {
-  description = "ARN of the bucket"
-  value       = module.rg.name
-}
-
-output "rglocation" {
-  description = "ARN of the bucket"
-  value       = module.rg.location
-}
-
 output "local1" {
   description = "A local variable"
   value       = local.flatstring
