dc6309cf7bb08dea0db87fa39559c04b3e49a280
Korey Gawronski <korey.gawronski@hpe.com>
2026-04-13 14:41:13 -0400

    Added outputs

--- Terraform/Azure/resourceGroup/outputs.tf
+++ Terraform/Azure/resourceGroup/outputs.tf
@@ -12,3 +12,18 @@ output "rglocation" {
   description = "ARN of the bucket"
   value       = module.rg.location
 }
+
+output "local1" {
+  description = "A local variable"
+  value       = local.flatstring
+}
+
+output "local2" {
+  description = "A local variable from tf vars"
+  value       = local.from_tf_vars
+}
+
+output "local3" {
+  description = "A local variable from morpheus vars"
+  value       = local.from_morpheus_vars
+}
